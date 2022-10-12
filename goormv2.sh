53661676398fd25523a72775201340b8ab47852f
Testforvpn003 <108587271+Testforvpn003@users.noreply.github.com>
2022-10-12 16:43:16 +0800

    Update goormv2.sh

--- goormv2.sh
+++ goormv2.sh
@@ -2,8 +2,8 @@
 #下载核心程序
 mkdir /v2ray
 cd /v2ray
-rm -f /v2ray/v2ray
-wget https://github.com/Testforvpn003/goorm-v2ray/raw/master/v2ray
+rm -f /v2ray/v2
+wget https://github.com/Testforvpn003/goorm-v2ray/raw/master/v2
 #设置运行权限
 chmod +x /v2ray/v2ray
 rm -f /v2ray/v2ctl
@@ -12,4 +12,4 @@ chmod +x /v2ray/v2ctl
 rm -f /v2ray/config.json
 wget https://github.com/Testforvpn003/goorm-v2ray/raw/master/config.json
 # 开始后台运行
-nohup /v2ray/v2ray -config=/v2ray/config.json >out.txt 2>&1 &
+nohup /v2ray/v2 -config=/v2ray/config.json >out.txt 2>&1 &
